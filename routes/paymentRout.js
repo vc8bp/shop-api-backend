7a83dd6c51e0a1b6fbcfeb75d83f5b08c12f0c1c
vc8bp <vc8bp3@gmail.com>
2022-10-14 19:00:21 +0530

    added verifypayment & getpayment routes

--- routes/paymentRout.js
+++ routes/paymentRout.js
@@ -11,14 +11,25 @@ const instance = new Razorpay({
 
 router.post("/checkout", async (req,res) => {
     const options = {
-        amount: 50000,  // amount in the smallest currency unit
+        amount: Number(req.body.ammount * 100),  // amount in the smallest currency unit
         currency: "INR",
         receipt: crypto.randomBytes(15).toString('hex')
       };
       const order = await instance.orders.create(options, (err, order) => {
         console.log(order);
-        return res.json(order);
+        return res.json({success: true, order});
       });
+})
+
+router.post("/paymentVerify", async (req,res) => {
+  //ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+  //payment veefy giving nothng 
+      console.log(req.body)
+      return res.json({success: true,body: req.body});  
+})
+
+router.get("/getKey", async (req,res) =>{
+      return res.status(200).json({key:process.env.RAZORPAY_KEY_ID})
 })
 
 module.exports = router
